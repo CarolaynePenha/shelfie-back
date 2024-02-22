8d1b4e802f2ed5b13caeebd0f94e4df31f19a8b9
Carolayne <carolaynepenha@gmail.com>
2024-02-22 11:38:10 -0300

    start

---
+++ src/middlewares/handleErrorsMiddleware.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction } from "express";
+
+const serviceErrorToStatusCode = {
+  unauthorized: 401,
+  notFound: 404,
+  conflict: 409,
+  badRequest: 400,
+};
+
+export function badRequestError(message: string) {
+  return { type: "badRequest", message };
+}
+export function unauthorizedError(message: string) {
+  return { type: "unauthorized", message };
+}
+
+export function notFoundError(message: string) {
+  return { type: "notFound", message };
+}
+export function conflictError(message: string) {
+  return { type: "conflict", message };
+}
+export default function handleErrorsMiddleware(
+  err,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  console.error(err);
+  if (err.type) {
+    return res.status(serviceErrorToStatusCode[err.type]).send(err.message);
+  }
+  return res.status(500).send(err);
+}
