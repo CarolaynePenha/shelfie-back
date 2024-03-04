f55f13754e7852ac2dea8b41a2221e8c74ba5017
Carolayne <carolaynepenha@gmail.com>
2024-03-04 19:45:16 -0300

    feat: AVG rating

--- src/repositories/bookRepository.ts
+++ src/repositories/bookRepository.ts
@@ -1,10 +1,15 @@
 import { prisma } from "../config/database.js";
 
 async function findBookById(id: number) {
-  const book = await prisma.book.findFirst({
-    include: { author: true },
-    where: { id },
-  });
+  const book = await prisma.$queryRaw`
+ SELECT books.*, authors.*, AVG(ratings.stars) as totalStars
+  FROM books
+  JOIN authors ON books."authorId" = authors.id
+  LEFT JOIN ratings ON books.id = ratings."bookId"
+  WHERE books.id = ${id}
+  GROUP BY books.id, authors.id
+`;
+
   return book;
 }
 
